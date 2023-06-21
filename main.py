9460a97e13eda556cee186f6be866b34a797eddf
steven duchene <steven.duchene@polymtl.ca>
2023-06-20 20:03:27 -0400

    v1 of portfolio

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/project1')
+def project1():
+    return render_template("project1.html")
+
+@app.route('/project2')
+def project2():
+    return render_template("project2.html")
+
+@app.route('/project3')
+def project3():
+    return render_template("project3.html")
+
+@app.route('/project4')
+def project4():
+    return render_template("project4.html")
+
+if __name__ == "__main__":
+    app.run(host='192.168.0.191', port=5000, debug=True)
